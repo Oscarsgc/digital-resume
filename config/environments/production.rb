4b2f8daf392851a78df2386629ebbc1f3aadf8e4
Oscar González Camacho <oscar_sgc@hotmail.com>
2016-04-24 20:58:59 -0400

    Expecting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  #config.serve_static_files = true
+  config.serve_static_files = true
   config.assets.compile = true
+  config.assets.precompile = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
